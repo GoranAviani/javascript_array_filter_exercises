8d648fa766846632d8a36f346ee2a2c6d59d90aa
goranaviani <goranaviani@gmail.com>
2022-06-18 07:07:15 +0200

    javascript_array_filter_exercises

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import fiveAndGreaterOnly from "./components/fiveAndGreaterOnly";
+import evensOnly from "./components/evensOnly";
 
 function App() {
     console.log(fiveAndGreaterOnly([3, 6, 8, 2])); /// [6, 8]
+    console.log(evensOnly([3, 6, 8, 2])); /// [6, 8, 2]
+
 
   return (
     <div className="App">
      APP
         <p>{fiveAndGreaterOnly([3, 6, 8, 2])}</p>
+        <p>{evensOnly([3, 6, 8, 2])}</p>
     </div>
   );
 }
