cede8a12b429faa4fe57c02718e47d5b0681e1bd
goranaviani <goranaviani@gmail.com>
2022-06-18 21:26:27 +0200

    javascript_array_filter_exercises

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import fiveAndGreaterOnly from "./components/fiveAndGreaterOnly";
 import evensOnly from "./components/evensOnly";
+import fiveCharactersOrFewerOnly from "./components/fiveCharactersOrFewerOnly";
 
 function App() {
     console.log(fiveAndGreaterOnly([3, 6, 8, 2])); /// [6, 8]
     console.log(evensOnly([3, 6, 8, 2])); /// [6, 8, 2]
+    console.log(fiveCharactersOrFewerOnly(["dog", "wolf", "by", "family", "eaten", "camping"])); // ["by", "dog", "wolf", "eaten"]
+
 
 
   return (
