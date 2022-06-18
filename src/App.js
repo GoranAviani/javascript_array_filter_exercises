259b4720f42e4db9a9bed1d3e2ce09adccf83196
goranaviani <goranaviani@gmail.com>
2022-06-18 21:31:30 +0200

    javascript_array_filter_exercises

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import './App.css';
 import fiveAndGreaterOnly from "./components/fiveAndGreaterOnly";
 import evensOnly from "./components/evensOnly";
 import fiveCharactersOrFewerOnly from "./components/fiveCharactersOrFewerOnly";
+import peopleWhoBelongToTheMovieClub from "./components/peopleWhoBelongToTheMovieClub";
 
 function App() {
     console.log(fiveAndGreaterOnly([3, 6, 8, 2])); /// [6, 8]
     console.log(evensOnly([3, 6, 8, 2])); /// [6, 8, 2]
     console.log(fiveCharactersOrFewerOnly(["dog", "wolf", "by", "family", "eaten", "camping"])); // ["by", "dog", "wolf", "eaten"]
-
+console.log(peopleWhoBelongToTheMovieClub([
+    { name: "Angelina Jolie", member: true },
+    { name: "Eric Jones", member: false },
+    { name: "Paris Hilton", member: false },
+    { name: "Kayne West", member: false },
+    { name: "Bob Ziroll", member: true }
+]));
+// =>
+//[ { name: 'Angelina Jolie', member: true },
+//  { name: 'Bob Ziroll', member: true } ]
 
 
   return (
