92c38020b576bc25c0a30a8bd0e59983b560f339
goranaviani <goranaviani@gmail.com>
2022-06-19 13:45:41 +0200

    javascript_array_filter_exercises

--- src/App.js
+++ src/App.js
@@ -8,6 +8,16 @@ function App() {
     console.log(fiveAndGreaterOnly([3, 6, 8, 2])); /// [6, 8]
     console.log(evensOnly([3, 6, 8, 2])); /// [6, 8, 2]
     console.log(fiveCharactersOrFewerOnly(["dog", "wolf", "by", "family", "eaten", "camping"])); // ["by", "dog", "wolf", "eaten"]
+console.log(peopleWhoBelongToTheMovieClub([
+    { name: "Angelina Jolie", member: true },
+    { name: "Eric Jones", member: false },
+    { name: "Paris Hilton", member: false },
+    { name: "Kayne West", member: false },
+    { name: "Bob Ziroll", member: true }
+]));
+// =>
+//[ { name: 'Angelina Jolie', member: true },
+//  { name: 'Bob Ziroll', member: true } ]
 
 
   return (
@@ -16,11 +26,6 @@ function App() {
         <p>{fiveAndGreaterOnly([3, 6, 8, 2])}</p>
         <p>{evensOnly([3, 6, 8, 2])}</p>
         <p>{fiveCharactersOrFewerOnly(["dog", "wolf", "by", "family", "eaten", "camping"])}</p>
-        <p>{peopleWhoBelongToTheMovieClub([{name: 'Wes', position: 'WR'}, {name: 'Coty', position: 'QB'}, {name: 'Joe', position: 'WR'}])}</p>
-
-
-
-
     </div>
   );
 }
